52f575fac1d42bb94b65b210d71b9a685d956c62
Phillip Lopo <phillip@lopo.io>
2019-01-25 14:14:53 -0800

    day10_1 solved

--- 10/day10_1.py
+++ 10/day10_1.py
@@ -1,7 +1,6 @@
 import re
 from dataclasses import dataclass
-import time
-import math
+from collections import Counter
 
 @dataclass(order=True)
 class Coordinate:
@@ -25,122 +24,70 @@ class Point:
         self.pos = Coordinate(px, py)
         self.v = Coordinate(vx, vy)
 
+    def update(self):
+        self.pos.x += self.v.x
+        self.pos.y += self.v.y
+
+
+def print_grid(points):
+    min_y = min(points, key=lambda x: x.pos.y).pos.y
+    max_y = max(points, key=lambda x: x.pos.y).pos.y
+    min_x = min(points, key=lambda x: x.pos.x).pos.x
+    max_x = max(points, key=lambda x: x.pos.x).pos.x
+
+    coords = [point.pos for point in points]
+
+    y = min_y
+    while y < max_y + 1:
+        x = min_x
+        while x < max_x + 1:
+            p = Coordinate(x, y)
+            if p in coords:
+                print('#', end='')
+            else:
+                print('.', end='')
+            x += 1
+        y += 1
+        print()
+
+
+def count_lines(points):
+    coords = {(point.pos.x, point.pos.y) for point in points}
+    min_y = min(coords, key=lambda c: c[1])[1]
+    max_y = max(coords, key=lambda c: c[1])[1]
+
+    counts = []
+    for c in coords:
+        x, y = c
+        if y > min_y:
+            continue
+
+        count = 0
+        while y < max_y + 1:
+            if (x, y) in coords:
+                count += 1
+            else:
+                break
+            y += 1
+
+        counts.append(count)
+    counts = Counter(counts)
+    max_length = max(counts.keys())
+    return max_length, counts[max_length]
+
 
 prog = re.compile(r'position=<(.+?),(.+?)> velocity=<(.+?),(.+?)>')
-with open('input.txt.example') as f:
+with open('input.txt') as f:
     points = [Point(px, py, vx, vy) for px, py, vx, vy in prog.findall(f.read())]
 
-
-def print_grid(grid, pretty=False):
-    for line in grid:
-        if pretty:
-            print(''.join(['.' if z == 0 else '#' for z in line]))
-        else:
-            print(''.join([str(z) for z in line]))
-    print()
-
-
 second = 0
-sizes = []
 while True:
-    min = Coordinate(points[0].pos.x, points[0].pos.y)
-    max = Coordinate(min.x, min.y)
-
-    for p in points:
-        if p.pos.x < min.x:
-            min.x = p.pos.x
-        if p.pos.y < min.y:
-            min.y = p.pos.y
-        if p.pos.x > max.x:
-            max.x = p.pos.x
-        if p.pos.y > max.y:
-            max.y = p.pos.y
-
-    size = Coordinate(abs(min.x) + abs(max.x), abs(min.y) + abs(max.y))
-    grid = [[0]*(size.x+1) for z in range(size.y+1)]
-    focal_sum = [[0]*(size.x+1) for z in range(size.y+1)]
-
-    for p in points:
-        x = p.pos.x + min.x
-        y = p.pos.y + min.y
-
-        grid[y][x] = 1
-
-    for y in range(size.y+1):
-        for x in range(size.x+1):
-            s = 0
-            if y-1 >= 0:
-                if x-1 >= 0:
-                    s += grid[y-1][x-1]
-                s += grid[y-1][x]
-                if x+1 <= size.x:
-                    s += grid[y-1][x+1]
-
-            if x-1 >= 0:
-                s += grid[y][x-1]
-            s += grid[y][x]
-            if x+1 <= size.x:
-                s += grid[y][x+1]
-
-            if y+1 <= size.y:
-                if x-1 >= 0:
-                    s += grid[y+1][x-1]
-                s += grid[y+1][x]
-                if x+1 <= size.x:
-                    s += grid[y+1][x+1]
-
-            focal_sum[y][x] = s
-
-    # print(min,  max)
-
-
-    # size = pow(abs(min.x) + abs(max.x),2) + pow(abs(min.y) + abs(max.y),2)
-    # size = Coordinate(abs(min.x) + abs(max.x), abs(min.y) + abs(max.y))
-    # print(f'second={second}', f'size={size}')
-    # print(size)
-    sizes.append(size)
-    # if size == 130:
-    #     print(f'second={second}')
-    #     for y in range(abs(min.y) + abs(max.y) + 1):
-    #         for x in range(abs(min.x) + abs(max.x) + 1):
-    #             coord = Coordinate(x + min.x, y + min.y)
-    #             match = False
-    #             for p in points:
-    #                 if p.pos == coord:
-    #                     # print(coord, p)
-    #                     # print('match!!!!')
-    #                     match = True
-    #             if match:
-    #                 print('#', end='')
-    #             else:
-    #                 print('.', end='')
-    #         print()
-    #     print()
-
-    s = 0
-    for row in focal_sum:
-        for col in row:
-            s += col
-
-
-
-    print(s)
-    print(1 + math.log(s))
-    num_coords = size.x * size.y
-    odds = 1/num_coords * 1/(num_coords-1) * 1/(num_coords-2) * 1/(num_coords-3) * 1/(num_coords-4)
-    print(size, num_coords, odds)
-
-    print_grid(grid, pretty=True)
-    print_grid(focal_sum, pretty=False)
-
-    # time.sleep(1)
-
-    second += 1
-    for p in points:
-        p.pos = p.pos + p.v
-
-    if second > 3:
+    count = count_lines(points)
+    if count[0] >= 5:
+        print_grid(points)
+        print(f'second={second}, lines={count}')
+        print()
         break
 
-sizes = sorted(sizes)[0:10]
-print(sizes)
+    for p in points:
+        p.update()
